8fcba35508609ba29f2b6fa4aa70869deaabb300
sunny <sunnygb.8@gmail.com>
2020-04-03 08:42:21 +0800

    change app title

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Awesome Star Wars Vehicles</h1>
+        <h1>Awesome Star Wars Vehicles --,</h1>
         <Router>
           <ul>
             <li><Link to="/">Home</Link></li>
